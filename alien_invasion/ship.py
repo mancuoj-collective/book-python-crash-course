f231e57fe117f59fa19b48ebdd1adf0a952c0207
mancuoj <mancuoj@gmail.com>
2024-09-11 23:03:05 +0800

    chore: update

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -8,7 +8,20 @@ class Ship:
 
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
+        self.x = float(self.rect.x)
+
         self.rect.midbottom = self.screen_rect.midbottom
+        self.speed = 1.5
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
